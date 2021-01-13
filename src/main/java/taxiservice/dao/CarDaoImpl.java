f2a219a41b0627db2886f83ff4d7302aa38d59ad
Tonitrus <grom4er@gmail.com>
2021-01-13 12:47:34 +0200

    Create and fill CarDaoImplm, DriverDaoImpl

--- src/main/java/taxiservice/dao/CarDaoImpl.java
+++ src/main/java/taxiservice/dao/CarDaoImpl.java
@@ -1,38 +1,59 @@
 package taxiservice.dao;
 
-import taxiservice.models.Car;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
+import taxiservice.db.Storage;
+import taxiservice.models.Car;
+import taxiservice.models.Driver;
 
-public class CarDaoImpl implements CarDao{
-    @Override
-    public List<Car> getAllByDriver(Long driverId) {
-        return null;
-    }
+public class CarDaoImpl implements CarDao {
 
     @Override
     public Car create(Car element) {
-        return null;
+        return Storage.addCarToStorageAndTakeId(element);
     }
 
     @Override
     public Optional<Car> get(Long id) {
-        return Optional.empty();
+        return Storage.getCarsStorage()
+                .stream()
+                .filter(x -> x.getId().equals(id))
+                .findFirst();
     }
 
     @Override
     public List<Car> getAll() {
-        return null;
+        return Storage.getCarsStorage();
     }
 
     @Override
     public Car update(Car element) {
-        return null;
+        IntStream.range(0, Storage.getCarsStorage().size())
+                .filter(i -> Storage.getCarsStorage()
+                        .get(i).getId().equals(element.getId()))
+                .findFirst()
+                .ifPresent(i -> Storage.getCarsStorage().set(i, element));
+        return element;
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        return Storage.getCarsStorage()
+                .removeIf(x -> x.getId().equals(id));
+    }
+
+    @Override
+    public List<Car> getAllByDriver(Long driverId) {
+        List<Car> carWithDrivers = new ArrayList<>();
+        for (Car car : Storage.getCarsStorage()) {
+            for (Driver driver : car.getDrivers()) {
+                if (driver.getId().equals(driverId)) {
+                    carWithDrivers.add(car);
+                }
+            }
+        }
+        return carWithDrivers;
     }
 }
---
+++ src/main/java/taxiservice/dao/DriverDao.java
@@ -0,0 +1,6 @@
+package taxiservice.dao;
+
+import taxiservice.models.Driver;
+
+public interface DriverDao extends GenericDao<Driver> {
+}
